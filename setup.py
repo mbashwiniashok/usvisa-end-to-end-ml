7acfcb62eb5dbd3bb3d53d99d7f6f0ab6e0e770c
abhishekk-fs <abhishek.k@factspan.com>
2024-03-19 11:15:21 +0530

    added requirements.txt

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.1.0",
+    author="Abhishek Kumar",
+    author_email="abhishek.k@factspan.com",
+    packages=find_packages()
+)
